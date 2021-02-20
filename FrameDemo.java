a9b5e9382e7c0aa1756f2e353c032f411d0e9299
Sean Brown <>
2021-02-20 10:05:02 -0500

    Added radio buttons and grouped them

--- FrameDemo.java
+++ FrameDemo.java
@@ -4,23 +4,35 @@ import javax.swing.*;
  
 /* FrameDemo.java requires no other files. */
 public class FrameDemo {
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    
+      
     
      private static void searchGui() {
         //Create and set up the window.
         JFrame frame = new JFrame("Search");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
+//************************************************************* */ 
         JTextField jTextField1 = new JTextField("");
         jTextField1.setBounds(100,100,200,30);
         frame.add(jTextField1);
 
+        JRadioButton jButton1 = new JRadioButton("All Terms");
+        jButton1.setBounds(50,150,100,30);
+        frame.add(jButton1);
 
+        JRadioButton jButton2 = new JRadioButton("Any Terms");
+        jButton2.setBounds(150,150,100,30);
+        frame.add(jButton2);
+
+        JRadioButton jButton3 = new JRadioButton("Exact Term");
+        jButton3.setBounds(250,150,100,30);
+        frame.add(jButton3);
+
+        ButtonGroup buttons = new ButtonGroup();
+        buttons.add(jButton1);
+        buttons.add(jButton2);
+        buttons.add(jButton3);
+
+//***************************************************************** */
         JButton a = new JButton("About");
         a.setBounds(250,350,100,30);
         frame.add(a);
@@ -58,10 +70,7 @@ public class FrameDemo {
   emptyLabel.setPreferredSize(new Dimension(400, 400));
   frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
 
-
-
-
-        
+       
         //Display the window.
         frame.pack();
         frame.setVisible(true);
@@ -70,9 +79,8 @@ public class FrameDemo {
      
 
     private static void aboutGui() {
-        
-              
-        
+            
+       
         //Create and set up the window.
         JFrame frameAbout = new JFrame("About");
         //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -87,9 +95,6 @@ public class FrameDemo {
         frameAbout.setVisible(true);
     }
  
-
-
-
     private static void maintenanceGui() {
         //Create and set up the window.
         JFrame frame = new JFrame("Maintenance");
@@ -104,9 +109,7 @@ public class FrameDemo {
         frame.setVisible(true);
     }
 
-    
-
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
