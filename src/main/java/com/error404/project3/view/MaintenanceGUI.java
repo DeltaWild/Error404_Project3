3de4d0080eb20a8fdcdae502cfc645ee5480ed32
Brianna MacKenzie <briannanmackenzie@gmail.com>
2021-03-23 14:33:48 -0400

    Formatted last modified date column in MaintenanceGUI table.

--- src/main/java/com/error404/project3/view/MaintenanceGUI.java
+++ src/main/java/com/error404/project3/view/MaintenanceGUI.java
@@ -81,6 +81,7 @@ class MaintenanceGUI {
         maintDisplayTable.setSelectionMode(0); // Single file selection only
         maintDisplayTable.setFillsViewportHeight(true); // use entire container
         maintDisplayTable.setAutoResizeMode(4); // Resize all columns
+        maintDisplayTable.setDefaultRenderer(Long.class, new DateRenderer());
         scrollPane.setViewportView(maintDisplayTable); // set maintDisplayTable to display in ScrollPane
         /* ***** */
 
---
+++ src/main/java/com/error404/project3/view/dateRenderer.java
@@ -0,0 +1,27 @@
+package com.error404.project3.view;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class DateRenderer extends DefaultTableCellRenderer {
+    DefaultTableCellRenderer tCR = new DefaultTableCellRenderer();
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+
+        Component comp = tCR.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        SimpleDateFormat sDF = new SimpleDateFormat("MM/dd/yyyy 'at' hh:mm:ss a z");
+
+        if (column == 2) {
+            Date modDate = new Date((Long) value);
+            String modDateConverted = sDF.format(modDate);
+            comp = tCR.getTableCellRendererComponent(table, modDateConverted, isSelected, hasFocus, row, column);
+        }
+
+        return comp;
+    }
+}
+/*  */
