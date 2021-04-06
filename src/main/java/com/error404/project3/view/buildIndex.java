ae3c88dfefdc0b043e7ff31063911439337428a0
Brianna MacKenzie <briannanmackenzie@gmail.com>
2021-04-06 16:52:05 -0400

    Added buildIndex.java. Added tableModelListener to fileList.java.
    Solution generates file "engine.txt" on adding files to index. Currently used for testing on branch; "engine.txt" will not be pushed.

--- src/main/java/com/error404/project3/view/buildIndex.java
+++ src/main/java/com/error404/project3/view/buildIndex.java
@@ -3,8 +3,7 @@ package com.error404.project3.view;
 import com.error404.project4.model.fileItem;
 
 import java.awt.*;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 import static com.error404.project3.view.MaintenanceGUI.maintDisplayTable;
@@ -14,9 +13,13 @@ public class buildIndex {
     public buildIndex() throws IOException {
         maintDisplayTable.setBackground(Color.BLUE);
         MaintenanceGUI.window.repaint();
-        FileWriter outFile = new FileWriter("engine.txt", StandardCharsets.UTF_8);
+        FileOutputStream fos = new FileOutputStream("engine.txt");
+        OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
         for (fileItem file : fileInfo) {
-            outFile.write(String.valueOf(file));
+            osw.write(file);
+            osw.flush();
         }
+        osw.close();
+        fos.close();
     }
 }
