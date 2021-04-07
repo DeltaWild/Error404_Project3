3658c7f31a396ad32d97ab178ceb44e54dfc6d1b
Brianna MacKenzie <briannanmackenzie@gmail.com>
2021-04-07 10:49:50 -0400

    Begin index implementation in buildIndex.java.

--- src/main/java/com/error404/project4/model/buildIndex.java
+++ src/main/java/com/error404/project4/model/buildIndex.java
@@ -2,33 +2,40 @@ package com.error404.project4.model;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 import static com.error404.project3.view.fileList.fileInfo;
 
 public class buildIndex {
     public buildIndex() throws IOException {
+        Set<String> results = new HashSet<>(); // store words of file here
+
         FileWriter fw = new FileWriter("engine.txt", StandardCharsets.UTF_8);
         BufferedWriter bw = new BufferedWriter(fw);
 
         // Print file list
         /* ***** */
-        for (fileItem file : fileInfo) {
+        for (fileItem file : fileInfo) { // for each file in fileInfo List:
             bw.write(file.fileID + "\t" +
                     file.fileName + "\t" +
                     file.fileSize + "\t" +
-                    file.fileModDate + "\n");
+                    file.fileModDate + "\n"); // print the details of each file, one file per line
         }
         /* ***** */
 
         // Generate inverted index for each file
-        for (fileItem file: fileInfo) {
-            FileReader fr = new FileReader(file.fileName);
-            BufferedReader br = new BufferedReader(fr);
+        for (fileItem file: fileInfo) { // for each file in fileInfo List:
+            File path = new File(file.fileName); // associate file.fileName with File element for Scanner
+            Scanner in = new Scanner(path); // Scanner to read file
 
+            while (in.hasNext()) { // iterate over file
+                results.add(in.next()); // add word to List
+            }
 
-
-            br.close();
-            fr.close();
+            for (String result : results) { // for each word in List:
+                bw.write(result + "\n"); // write word to output file followed by newline
+            }
+            in.close();
         }
 
         bw.close();
